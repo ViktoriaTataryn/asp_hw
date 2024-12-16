35bdb66e570ab279273ed7668c1f71f5f0b862a6
ViktoriaTataryn <viktoriatatarin252@gmail.com>
2024-12-16 19:52:35 +0200

    pract5

---
+++ pract5/pract5/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pract5.Models;
+
+namespace pract5.Controllers;
+[ApiController]
+[Route("users")]
+
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<List<User>> GetUsers()
+    {
+        var users = new List<User>()
+        {
+            new User()
+            { 
+                Id = 1,
+                Nickname="user1",
+                Password = "user123",
+                DateOfBirth ="12.03.2001",
+                Description="User1"
+            },
+             new User()
+            {
+                Id = 2,
+                Nickname="user2",
+                Password = "user456",
+                DateOfBirth ="09.06.2003",
+                Description="User2"
+            },
+    };
+        return Ok(users);
+        }
+
+    [HttpGet("{Nickname}")]
+    public ActionResult<User> GetUsersByNickname([FromRoute] string Nickname)
+    {
+        var user = new User() {
+            Id = 3,
+            Nickname = Nickname,
+            Password = "user456",
+            DateOfBirth = "09.06.2003",
+            Description = "User2"
+        };
+        return Ok(user);
+    }
+    [HttpPost]
+    public ActionResult<User> AddUser([FromBody] User user)
+    {
+        user.Id = new Random().Next(1, 1000000);
+        return Created($"/users/{user.Id}", user);
+    }
+}
+
---
+++ pract5/pract5/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace pract5.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Nickname { get; set; }
+        public string Password { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Description { get; set; }
+    }
+}
